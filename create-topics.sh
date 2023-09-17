c929638a1a6aa8db365859187cd7b8f6783ca608
João Eduardo Ferreira de Lima <joao.ferreira@csdbr.com>
2023-09-17 09:32:52 -0300

    primeira implementacao do k8s

--- create-topics.sh
+++ create-topics.sh
@@ -5,12 +5,12 @@
 /opt/bitnami/scripts/kafka/run.sh &
 
 # Aguarda o servidor Kafka iniciar
-sleep 20
+sleep 40
 
 # Cria tópicos
-kafka-topics.sh --create --if-not-exists --bootstrap-server host.docker.internal:9092 --topic comando.radar
-kafka-topics.sh --create --if-not-exists --bootstrap-server host.docker.internal:9092 --topic multa
-kafka-topics.sh --create --if-not-exists --bootstrap-server host.docker.internal:9092 --topic relatorio
+kafka-topics.sh --create --if-not-exists --bootstrap-server kafka:9092 --topic comando.radar
+kafka-topics.sh --create --if-not-exists --bootstrap-server kafka:9092 --topic multa
+kafka-topics.sh --create --if-not-exists --bootstrap-server kafka:9092 --topic relatorio
 
 # Mantem o contêiner em execução
 tail -f /dev/null
